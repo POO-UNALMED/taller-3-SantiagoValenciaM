bf9159519f1e4c18af4966a8e6a8252b627abfce
Santiago Valencia Mejía <69688207+SantiagoValenciaM@users.noreply.github.com>
2020-09-03 15:09:00 -0500

    Add files via upload

---
+++ src/main/java/taller3/televisores/Marca.java
@@ -0,0 +1,14 @@
+package taller3.televisores;
+
+public class Marca {
+	private String nombre;
+	public Marca(String nomb){
+		setNombre(nomb);
+	}
+	public String getNombre () {
+		return nombre;
+	}
+	public  void setNombre(String nombre ) {
+		this.nombre=nombre;
+	}
+}
